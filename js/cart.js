38384135ce5fe6713062592793d7dc845a418ecc
ZOBI <emekaezeobi@outlook.com>
2023-08-29 07:14:36 +0100

    Onclick

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,12 @@
 let cartQuantity = 0;
+
+//const message = 'Hello, World!';
+//console.log(typeof message);
+
+const cost = (500 + (2 * 300) + 900) /100;
+const taxCost = cost * 0.1;
+const foodCost = cost + taxCost;
+console.log(`Cost of Food: $${foodCost}`);
+
+
+
